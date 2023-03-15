14946582eb6ede2858d01e65fd9d7b140144080a
anneu <b.imnuj@gmail.com>
2023-03-15 14:44:26 +0900

    feat : 프로젝트 뼈대 추가

---
+++ back/blog/src/test/java/saramdle/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package saramdle.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
