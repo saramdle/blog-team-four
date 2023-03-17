e3a6ab0c670c1a382f6c8170787af7181734a573
anneu <b.imnuj@gmail.com>
2023-03-17 12:06:42 +0900

    test : 게시물 단건 조회 기능 테스트

--- back/blog/src/test/java/saramdle/blog/service/PostServiceTest.java
+++ back/blog/src/test/java/saramdle/blog/service/PostServiceTest.java
@@ -6,13 +6,19 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 import saramdle.blog.domain.Post;
+import saramdle.blog.domain.PostRepository;
 
+@Transactional
 @SpringBootTest
 class PostServiceTest {
 
     @Autowired
     PostService postService;
+
+    @Autowired
+    PostRepository postRepository;
 
     @Test
     @DisplayName("게시물을 저장합니다.")
@@ -20,5 +26,16 @@ class PostServiceTest {
         Post post = Post.builder().build();
         Long savedId = postService.save(post);
         assertThat(savedId).isEqualTo(post.getId());
+    }
+
+    @Test
+    @DisplayName("하나의 게시물을 조회합니다.")
+    void findPost() {
+        Post post = Post.builder().build();
+        postRepository.save(post);
+
+        Post findPost = postService.findPost(post.getId());
+
+        assertThat(findPost).isEqualTo(post);
     }
 }
