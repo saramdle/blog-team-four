f400b4c16ae994c6e3e91edb8edde9eaf5480ff5
howooking <junsgk@gmail.com>
2023-03-18 11:58:03 +0900

    feat : 커스텀 error 페이지
    - 에러 발생 시 해당 페이지로 이동

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -1,4 +1,4 @@
-import { Metadata } from "next";
+import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
