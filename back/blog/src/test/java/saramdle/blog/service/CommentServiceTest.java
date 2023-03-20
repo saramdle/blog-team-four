9dc880d0600f44b188dca47859ad3271ef8b7e55
sangwooyu <dbtkddn598@gmail.com>
2023-03-20 17:09:49 +0900

    test : Comment Create 기능 테스트 수정
    - 변경 전) comment post 맵핑 없이 DB에 저장  
    - 변경 후) comment - post 맵핑 후 comment에 post가 주입되어져서 DB에 저장

--- back/blog/src/test/java/saramdle/blog/service/CommentServiceTest.java
+++ back/blog/src/test/java/saramdle/blog/service/CommentServiceTest.java
@@ -1,24 +1,44 @@
 package saramdle.blog.service;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import saramdle.blog.domain.Comment;
+import saramdle.blog.domain.Post;
+
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class CommentServiceTest {
+
     @Autowired
     CommentService commentService;
+
+    @Autowired
+    PostService postService;
+
+
+    @BeforeEach
+    void beforeEach(){
+        Post post1 = Post.builder().build();
+        Long savedId1 = postService.save(post1);
+        Assertions.assertThat(post1.getId()).isEqualTo(savedId1);
+    }
 
     @DisplayName("댓글 저장")
     @Test
     void save() {
-        Comment comment = Comment.builder().build();
+        Post post = Post.builder().build();
+        postService.save(post);
+
+        Comment comment = Comment.builder().post(post).build();
         Long saveId = commentService.save(comment);
+
         assertThat(saveId).isEqualTo(comment.getId());
     }
 
