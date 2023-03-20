c97a5be19bd9a2045c7e4bc202def6d208b598b3
howooking <junsgk@gmail.com>
2023-03-20 20:15:42 +0900

    feat : 프리뷰 카드 디자인 수정
    - 불필요한 정보삭제, 구도 정리

--- front/app/components/PostPrevieCard.tsx
+++ front/app/components/PostPrevieCard.tsx
@@ -25,17 +25,14 @@ export default function PostPreviewCard({
             priority
           />
         </div>
-        <div className='flex flex-[1] flex-col items-center justify-center'>
+        <div className='flex flex-[1] flex-col items-center justify-center gap-5 p-2'>
+          <h2 className='my-auto px-9 font-bold text-neutral sm:text-xl'>
+            {title}
+          </h2>
           <div className='flex items-center gap-2 text-sm text-gray-400'>
-            <div className='h-9 w-9 overflow-hidden rounded-full'>
-              <Image alt='' src='/dummy_profile.avif' width={40} height={40} />
-            </div>
-            <div className='flex flex-col'>
-              <div>{author}</div>
-              <div>{moment(createdAt).format("YYYY MM DD")}</div>
-            </div>
+            <div>{author}</div>
+            <div>{moment(createdAt).format("YYYY-MM-DD")}</div>
           </div>
-          <h2 className='font-bold text-neutral sm:text-xl'>{title}</h2>
         </div>
       </div>
     </Link>
