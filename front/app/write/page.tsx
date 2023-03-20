e27147707423bd0a4fe5ae178b31ee78ea2d340d
howooking <junsgk@gmail.com>
2023-03-20 11:41:09 +0900

    refactor : react quill 설정 변경
    ㅇ
    
    ㅇ
    1

--- front/app/write/page.tsx
+++ front/app/write/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, useMemo } from "react";
 import "react-quill/dist/quill.snow.css";
 
 export default function Write() {
@@ -22,17 +22,39 @@ export default function Write() {
   const [contentsInput, setContentsInput] = useState<string>("");
   const [title, setTitle] = useState<string>("");
 
+  const modules = useMemo(
+    () => ({
+      toolbar: {
+        container: [
+          [{ header: 1 }, { header: 2 }],
+          ["bold", "italic", "underline", "strike", "blockquote"],
+          [{ size: ["small", false, "large", "huge"] }, { color: [] }],
+          [{ list: "ordered" }, { list: "bullet" }],
+          ["image"],
+        ],
+        handlers: {
+          //이미지 업로드 함수 필요
+          // image: imageHandler,
+        },
+      },
+    }),
+    []
+  );
+
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     const postingData = {
       title,
+
       // 로그인된 user로 대체될 예정
       author: "홍길동",
       contents: contentsInput,
+
       // 업로드된 사진 url로 대체될 예정
       imgUrl:
         "https://images.unsplash.com/photo-1606787366850-de6330128bfc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
-      // db자동생성될 예정
+
+      // db에서 자동생성될 예정
       createdAt: new Date(),
       updatedAt: new Date(),
     };
@@ -46,25 +68,9 @@ export default function Write() {
     return response.json();
   };
 
-  const modules = {
-    toolbar: [
-      ["bold", "italic", "underline", "strike", "image"],
-      ["blockquote"],
-      [{ header: 1 }, { header: 2 }],
-      [{ list: "ordered" }, { list: "bullet" }],
-      [{ size: ["small", false, "large", "huge"] }],
-      [{ color: [] }, { background: [] }],
-      [{ align: [] }],
-      ["clean"],
-    ],
-  };
   return (
     <div className='flex'>
-      <form
-        method='POST'
-        className='flex flex-1 flex-col gap-5 p-4'
-        onSubmit={handleSubmit}
-      >
+      <form className='flex flex-1 flex-col gap-5 p-4' onSubmit={handleSubmit}>
         <div className='flex items-center'>
           <input
             type='text'
@@ -74,21 +80,16 @@ export default function Write() {
             onChange={(e) => setTitle(e.target.value)}
           />
         </div>
-        <label
-          htmlFor='image'
-          className='w-max cursor-pointer text-gray-400 hover:text-primary'
-        >
-          이미지 업로드
-        </label>
-        <input type='file' name='image' id='image' className='hidden' />
+
         <div>
           {mounted && (
             <ReactQuill
               theme='snow'
               value={contentsInput}
               onChange={setContentsInput}
-              className='h-[500px]'
+              className='h-[550px]'
               modules={modules}
+              placeholder='배부르지 않으면 세상 모든 것이 어렵다...'
             />
           )}
         </div>
