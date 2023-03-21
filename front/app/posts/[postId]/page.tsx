e0e1868fc6a80ea6c1f99987525877b3b9cc605f
howooking <junsgk@gmail.com>
2023-03-21 10:53:40 +0900

    feat : 개별 포스트 디자인 수정
    - 사진 위치, title 위치 등 수정
    - 댓글 공간 분리

--- front/app/posts/[postId]/page.tsx
+++ front/app/posts/[postId]/page.tsx
@@ -1,4 +1,5 @@
 import Container from "@/app/components/Container";
+import moment from "moment";
 import { Metadata } from "next";
 import Image from "next/image";
 
@@ -33,21 +34,32 @@ export default async function Page({
   const postId = params.postId;
   const post: Post = await getPost(postId);
   return (
-    <Container>
-      <h1>{post?.title}</h1>
-      <Image
-        alt={post?.title || "food image"}
-        src={post?.imgUrl || ""}
-        width={300}
-        height={300}
-      />
-      <p
-        dangerouslySetInnerHTML={{ __html: post?.contents }}
-        className='prose max-w-none prose-p:m-0'
-      />
-      <p>{post?.author}</p>
-      <p>{post?.createdAt}</p>
-    </Container>
+    <>
+      <div className='main-img-ratio relative overflow-hidden'>
+        <div className='absolute z-10 flex h-full w-full flex-col items-center justify-center gap-10 font-bold'>
+          <h1 className='text-3xl text-gray-200'>{post?.title}</h1>
+          <div className='flex gap-4 text-gray-200'>
+            <div>{post?.author}</div>
+            <div>{moment(post?.createdAt).format("YYYY-MM-DD")}</div>
+          </div>
+        </div>
+        <Image
+          alt='home-image'
+          src={post?.imgUrl || ""}
+          className='object-cover'
+          fill
+        />
+        <div className='absolute inset-0 bg-black/40' />
+      </div>
+      <Container>
+        <p
+          dangerouslySetInnerHTML={{ __html: post?.contents }}
+          className='prose max-w-none py-9 prose-p:m-0'
+        />
+        <div className='h-1 bg-primary' />
+        <h3 className='text-center text-3xl'>Comments</h3>
+      </Container>
+    </>
   );
 }
 
