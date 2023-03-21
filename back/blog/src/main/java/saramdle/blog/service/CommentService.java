8291542a631c447be8827343111c0d193b41d11f
sangwooyu <dbtkddn598@gmail.com>
2023-03-21 13:54:28 +0900

    feat : 댓글ID로 댓글 조회 기능 구현
    - 댓글 고유 ID에 따른 댓글 데이터 조회 기능
    - Null일 경우 NOT_FOUND_COMMENT 에러 발생처리

--- back/blog/src/main/java/saramdle/blog/service/CommentService.java
+++ back/blog/src/main/java/saramdle/blog/service/CommentService.java
@@ -10,10 +10,15 @@ import saramdle.blog.domain.CommentRepository;
 @RequiredArgsConstructor
 public class CommentService {
 
+    private static final String NOT_FOUND_COMMENT = "해당 댓글을 찾을 수 없습니다.";
     private final CommentRepository repository;
 
     @Transactional
-    public Long save(Comment comment){
-        return repository.save(comment).getId();
+    public Long save(Comment comment) {
+        return repository.save(comment).getId();}
+
+    @Transactional(readOnly = true)
+    public Comment findComment(Long id) {
+        return repository.findById(id).orElseThrow(() -> new IllegalStateException(NOT_FOUND_COMMENT));
     }
 }
