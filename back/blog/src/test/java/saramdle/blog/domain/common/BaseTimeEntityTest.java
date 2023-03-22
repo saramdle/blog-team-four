29b92e42abd147e3db2cc184999eeba667cc1b9d
anneu <b.imnuj@gmail.com>
2023-03-22 20:13:45 +0900

    test : 수정 시간을 자동으로 변경하는 기능 테스트

--- back/blog/src/test/java/saramdle/blog/domain/common/BaseTimeEntityTest.java
+++ back/blog/src/test/java/saramdle/blog/domain/common/BaseTimeEntityTest.java
@@ -2,6 +2,7 @@ package saramdle.blog.domain.common;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,5 +43,37 @@ class BaseTimeEntityTest {
 
         assertThat(findComment.getCreatedAt()).isNotNull();
         assertThat(findComment.getUpdatedAt()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("게시물을 수정하면 수정시간이 자동으로 변경된다.")
+    void updateTimeOfPost() {
+        Post post = Post.builder().title("제목1").contents("내용1").build();
+        Long postId = postService.save(post);
+        Post oldPost = postService.findPost(postId);
+
+        Post updatePost = Post.builder().title("제목2").contents("내용2").build();
+        postService.update(postId, updatePost);
+        Post newPost = postService.findPost(postId);
+
+        LocalDateTime oldTime = oldPost.getUpdatedAt();
+        LocalDateTime newTime = newPost.getUpdatedAt();
+        assertThat(newTime).isAfter(oldTime);
+    }
+
+    @Test
+    @DisplayName("댓글을 수정하면 수정시간이 자동으로 변경된다.")
+    void updateTimeOfComment() {
+        Comment comment = Comment.builder().contents("내용1").build();
+        Long commentId = commentService.save(comment);
+        Comment oldComment = commentService.findComment(commentId);
+
+        Comment updateComment = Comment.builder().contents("내용2").build();
+        commentService.updateComment(commentId, updateComment);
+        Comment newComment = commentService.findComment(commentId);
+
+        LocalDateTime oldTime = oldComment.getUpdatedAt();
+        LocalDateTime newTime = newComment.getUpdatedAt();
+        assertThat(newTime).isAfter(oldTime);
     }
 }
