a6e52d21944dfe2772db1054f0cacd9e0f550595
anneu <b.imnuj@gmail.com>
2023-03-24 14:49:20 +0900

    feat : 게시물 상세 조회 요청을 처리하는 컨트롤러 구현

--- back/blog/src/main/java/saramdle/blog/controller/PostController.java
+++ back/blog/src/main/java/saramdle/blog/controller/PostController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -47,6 +48,14 @@ public class PostController {
                 .collect(Collectors.toList());
 
         return ResponseEntity.ok(dtos);
+    }
+
+    @GetMapping
+    @RequestMapping("/{id}")
+    public ResponseEntity<PostResponseDto> detail(@PathVariable long id) {
+        Post post = postService.findPost(id);
+        PostResponseDto dto = toDto(post);
+        return ResponseEntity.ok(dto);
     }
 
     private PostResponseDto toDto(Post post) {
