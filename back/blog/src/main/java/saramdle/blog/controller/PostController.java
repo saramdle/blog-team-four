b763785779a20031c850123da2b675e70c7efeb0
anneu <b.imnuj@gmail.com>
2023-03-24 17:14:39 +0900

    feat : 게시물 삭제 요청을 처리하는 컨트롤러 구현

--- back/blog/src/main/java/saramdle/blog/controller/PostController.java
+++ back/blog/src/main/java/saramdle/blog/controller/PostController.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -63,6 +64,12 @@ public class PostController {
         Long postId = postService.update(id, toEntity(postRequestDto));
         Post post = postService.findPost(postId);
         return ResponseEntity.ok(toDto(post));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable long id) {
+        postService.delete(id);
+        return ResponseEntity.ok("게시물 삭제 성공");
     }
 
     private PostResponseDto toDto(Post post) {
