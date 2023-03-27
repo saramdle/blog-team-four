6f6b126f7e89e4bfa17d107e24bbf4839a177ba6
anneu <b.imnuj@gmail.com>
2023-03-27 19:55:37 +0900

    feat : 예외 클래스 생성
    - 모든 커스텀 예외의 상위 클래스가 되는 BusinessException 예외 클래스 생성
    - 404 상태 코드를 뱉는 NotFoundException 생성

---
+++ back/blog/src/main/java/saramdle/blog/domain/exception/NotFoundException.java
@@ -0,0 +1,9 @@
+package saramdle.blog.domain.exception;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+public class NotFoundException extends BusinessException {
+    public NotFoundException(String message) {
+        super(message, NOT_FOUND);
+    }
+}
