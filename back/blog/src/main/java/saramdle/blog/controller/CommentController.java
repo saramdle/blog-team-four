c02c92e263f1c30c5884ac1f9b216106f23ee90e
sangwooyu <dbtkddn598@gmail.com>
2023-03-27 22:14:14 +0900

    feat : 댓글 목록 조회 요청을 처리하는 컨트롤러 구현

--- back/blog/src/main/java/saramdle/blog/controller/CommentController.java
+++ back/blog/src/main/java/saramdle/blog/controller/CommentController.java
@@ -3,17 +3,17 @@ package saramdle.blog.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import saramdle.blog.domain.Comment;
 import saramdle.blog.domain.CommentRequestDto;
+import saramdle.blog.domain.CommentResponseDto;
 import saramdle.blog.service.CommentService;
 import saramdle.blog.service.PostService;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
@@ -22,11 +22,20 @@ public class CommentController {
     private final CommentService commentService;
     private final PostService postService;
 
+    @GetMapping("/{postId}")
+    @ResponseBody
+    public ResponseEntity<List<CommentResponseDto>> getComment(@PathVariable Long postId) {
+        List<Comment> comments = commentService.findComments(postId);
+        List<CommentResponseDto> dtos = comments.stream()
+                .map(this::toDto)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(dtos);
+    }
+
     @PostMapping
     @ResponseBody
     public ResponseEntity<String> postComment(@RequestBody CommentRequestDto commentRequestDto) {
         Comment comment = toEntity(commentRequestDto);
-
         Long commentId = commentService.save(comment);
 
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
@@ -42,6 +51,15 @@ public class CommentController {
                 .contents(commentRequestDto.getContents())
                 .author(commentRequestDto.getAuthor())
                 .post(postService.findPost(commentRequestDto.getPostId()))
+                .build();
+    }
+
+    private CommentResponseDto toDto(Comment comment) {
+        return CommentResponseDto.builder()
+                .id(comment.getId())
+                .contents(comment.getContents())
+                .author(comment.getAuthor())
+                .createdAt(comment.getCreatedAt())
                 .build();
     }
 }
