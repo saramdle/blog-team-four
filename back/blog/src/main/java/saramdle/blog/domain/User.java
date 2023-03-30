c204f52a8c22ca7d38eda7fe82ba7f462c7d95a1
anneu <b.imnuj@gmail.com>
2023-03-30 17:44:44 +0900

    feat : 사용자 엔티티 생성
    - "User"는 데이터베이스 예약어이기 때문에 테이블의 이름을 "Users"로 지정해줌

---
+++ back/blog/src/main/java/saramdle/blog/domain/User.java
@@ -0,0 +1,45 @@
+package saramdle.blog.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import saramdle.blog.domain.common.BaseTimeEntity;
+
+@Entity
+@Getter
+@Table(name = "Users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String name;
+
+    private String picture;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Builder
+    private User(String email, String name, String picture, Role role) {
+        this.email = email;
+        this.name = name;
+        this.picture = picture;
+        this.role = role;
+    }
+}
