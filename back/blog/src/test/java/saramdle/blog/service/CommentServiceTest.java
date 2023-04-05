d91d7ddb3f03acfbc0fb178b09bdc2cb276b9e77
anneu <b.imnuj@gmail.com>
2023-04-05 22:02:46 +0900

    test : 댓글 서비스 테스트에 트랜잭션 적용
    - 각각의 테스트 환경은 격리되어야 하기 때문에 테스트가 끝나면 롤백하도록 트랜잭션 애노테이션 적용

--- back/blog/src/test/java/saramdle/blog/service/CommentServiceTest.java
+++ back/blog/src/test/java/saramdle/blog/service/CommentServiceTest.java
@@ -7,10 +7,12 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 import saramdle.blog.domain.Comment;
 import saramdle.blog.domain.Post;
 import saramdle.blog.domain.exception.NotFoundException;
 
+@Transactional
 @SpringBootTest
 class CommentServiceTest {
 
