0ea88e5c4f58ef2d7679601590da5c924c4710f0
howooking <junsgk@gmail.com>
2023-04-09 08:18:51 +0900

    refactor  : 개별 게시물 GET 요청 client side rendering으로 전환
    - react-query로 진행
    - author, createdAt을 <MainImage />컴포넌트에 같이 보여줌

--- front/app/components/MainImage.tsx
+++ front/app/components/MainImage.tsx
@@ -3,20 +3,25 @@ import Image, { StaticImageData } from "next/image";
 type Props = {
   title: string;
   src: StaticImageData | string;
+  createdAt?: string;
+  author?: string;
 };
 
-export default function MainImage({ title, src }: Props) {
+export default function MainImage({ title, src, author, createdAt }: Props) {
   return (
-    <div className='main-img-ratio relative overflow-hidden'>
-      <h1 className='absolute z-10 flex h-full w-full items-center justify-center font-bold text-white sm:text-3xl'>
-        {title}
-      </h1>
+    <div className='main-img-ratio relative mb-4 overflow-hidden'>
+      <div className='absolute z-10 flex h-full w-full flex-col items-center justify-center gap-3 p-10 font-bold text-white'>
+        <h1 className='my-auto text-4xl'>{title}</h1>
+        <h2 className='text-2xl'>{author}</h2>
+        <p>{createdAt?.slice(0, 10)}</p>
+      </div>
       <Image
         alt='home-image'
         src={src}
         fill
         sizes='50vw'
         className='object-cover'
+        priority
       />
       <div className='absolute inset-0 bg-black/40' />
     </div>
--- front/app/posts/[postId]/page.tsx
+++ front/app/posts/[postId]/page.tsx
@@ -1,48 +1,41 @@
+"use client";
+
 import Container from "@/app/components/Container";
 import MainImage from "@/app/components/MainImage";
-import { Metadata } from "next";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { usePathname } from "next/navigation";
 import CommentInput from "./CommentInput";
 import Comments from "./Comments";
 
-async function getPost(id: string) {
-  const res = await fetch(`http://localhost:4000/posts/${id}`);
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-  return res.json();
-}
+export default function Page() {
+  const pathName = usePathname();
+  const postId = pathName.split("/")[2];
+  const getPost = () => axios.get(`http://localhost:4000/posts/${postId}`);
+  const { isLoading, data, error } = useQuery({
+    queryKey: [`posts/${postId}`],
+    queryFn: getPost,
+  });
+  const post = data?.data;
 
-export async function generateMetadata({
-  params,
-}: {
-  params: {
-    postId: string;
-  };
-}): Promise<Metadata> {
-  const post = await getPost(params.postId);
-  return { title: post.title };
-}
+  if (error) error;
 
-export default async function Page({
-  params,
-}: {
-  params: {
-    postId: string;
-  };
-}) {
-  const postId = params.postId;
-  const post: Post = await getPost(postId);
+  if (isLoading) return null;
 
   return (
     <>
-      <MainImage src={post?.imgUrl} title={post?.title} />
+      <MainImage
+        src={post?.imgUrl}
+        title={post?.title}
+        createdAt={post?.createdAt}
+        author={post?.author}
+      />
       <Container>
         <p
           dangerouslySetInnerHTML={{ __html: post?.contents }}
           className='prose max-w-none prose-p:m-0'
         />
-        <p>{post?.author}</p>
-        <p>{post?.createdAt}</p>
+        <p>{}</p>
         <CommentInput postId={postId} />
         <Comments postId={postId} />
       </Container>
@@ -50,13 +43,13 @@ export default async function Page({
   );
 }
 
-export async function generateStaticParams() {
-  const res = await fetch("http://localhost:4000/posts");
-  if (!res.ok) {
-    throw new Error("Failed to fetch all the posts");
-  }
-  const posts: Post[] = await res.json();
-  return posts.map((post) => ({
-    postId: post.id.toString(),
-  }));
-}
+// export async function generateStaticParams() {
+//   const res = await fetch("http://localhost:4000/posts");
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch all the posts");
+//   }
+//   const posts: Post[] = await res.json();
+//   return posts.map((post) => ({
+//     postId: post.id.toString(),
+//   }));
+// }
