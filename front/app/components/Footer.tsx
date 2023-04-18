a5f6832790f7f7fc65820ba25ac1a8e4c8e777c4
howooking <junsgk@gmail.com>
2023-04-18 11:18:55 +0900

    refactor : "/write"페이지에서 header와 footer보이지 않게 수정
    - 글작성 과정에 불필요한 layouy(header, footer)가 보이지 않도록 수정했습니다.

--- front/app/components/Footer.tsx
+++ front/app/components/Footer.tsx
@@ -1,9 +1,24 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import Container from "./Container";
 
 export default function Footer() {
+  const path = usePathname();
+  if (path === "/write") {
+    return <></>;
+  }
   return (
-    <footer className='bg-slate-300'>
-      <Container>4조 블로그</Container>
+    <footer className='bg-slate-900 text-white'>
+      <Container>
+        <Link
+          href='/'
+          className='m-1 block w-60 border-4 border-white p-2 text-center text-sm font-bold uppercase text-white transition-colors duration-300 ease-in-out'
+        >
+          flavour 4 Favor
+        </Link>
+      </Container>
     </footer>
   );
 }
