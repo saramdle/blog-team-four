ac5500900ae877af8db13ec9049f5b8f305d6873
jade-coding <dbtkddn598@gmail.com>
2023-04-29 14:49:42 +0900

    feat : 이미지 S3 업로드 기능 구현

---
+++ back/blog/src/main/java/saramdle/blog/service/S3UploaderService.java
@@ -0,0 +1,99 @@
+package saramdle.blog.service;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.UUID;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Component
+public class S3UploaderService {
+
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @Value("${upload.dir}")
+    private String localDirUrl;
+
+    public String upload(MultipartFile multipartFile, String dirName) throws IOException {
+
+        File uploadFile = (File) convert(multipartFile).orElseThrow(()->new IllegalArgumentException("error: MultipartFile -> File convert fail"));
+
+        String fileName = dirName + "/" + UUID.randomUUID() + uploadFile.getName();   // S3에 저장된 파일 이름
+        String uploadImageUrl = putS3(uploadFile, fileName); // s3로 업로드
+        removeNewFile(uploadFile);
+
+        return uploadImageUrl;
+    }
+
+    // S3로 업로드
+    private String putS3(File uploadFile, String fileName) {
+        amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, uploadFile).withCannedAcl(CannedAccessControlList.PublicRead));
+        return amazonS3Client.getUrl(bucket, fileName).toString();
+    }
+
+    // 로컬에 저장된 이미지 지우기
+    private void removeNewFile(File targetFile) {
+        if (targetFile.delete()) {
+            log.info("File delete success");
+            return;
+        }
+        log.info("File delete fail");
+    }
+
+    /**
+     * @param multipartFile 로컬에 파일 저장하기
+     */
+    private Optional<Object> convert(MultipartFile multipartFile) throws IOException {
+        if (multipartFile.isEmpty()) {
+            return Optional.empty();
+        }
+        String fileDir = this.localDirUrl;
+
+        String originalFilename = multipartFile.getOriginalFilename();
+        String storeFileName = createStoreFileName(originalFilename);
+
+        //파일 업로드
+        File file = new File(fileDir+storeFileName);
+        multipartFile.transferTo(file);
+
+        return Optional.of(file);
+    }
+
+    /**
+     * @description 파일 이름이 이미 업로드된 파일들과 겹치지 않게 UUID를 사용한다.
+     * @param originalFilename 원본 파일 이름
+     * @return 파일 이름
+     */
+    private String createStoreFileName(String originalFilename) {
+        String ext = extractExt(originalFilename);
+        String uuid = UUID.randomUUID().toString();
+        return uuid + "." + ext;
+    }
+
+    /**
+     * @description 사용자가 업로드한 파일에서 확장자를 추출한다.
+     *
+     * @param originalFilename 원본 파일 이름
+     * @return 파일 확장자
+     */
+    private String extractExt(String originalFilename) {
+        int pos = originalFilename.lastIndexOf(".");
+        return originalFilename.substring(pos + 1);
+    }
+
+}
